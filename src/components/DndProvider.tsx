f1f91dc33049da8c21ee905d38bfb64dba8efb4f
Stasshe <egnm9stasshe@gmail.com>
2026-03-13 13:20:53 +0000

    feat: update DndProvider options for improved touch event handling

--- src/components/DndProvider.tsx
+++ src/components/DndProvider.tsx
@@ -24,7 +24,7 @@ export function DndProvider({ children }: DndProviderProps) {
   return (
     <ReactDndProvider
       backend={isTouch ? TouchBackend : HTML5Backend}
-      options={isTouch ? { enableMouseEvents: true, delay: 0, delayTouchStart: 0 } : undefined}
+      options={isTouch ? { enableMouseEvents: true, delayTouchStart: 250, ignoreContextMenu: true } : undefined}
     >
       {children}
     </ReactDndProvider>
